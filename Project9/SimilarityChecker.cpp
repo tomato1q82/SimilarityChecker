f0aa05343c51bcd37dbe9ad1a8f423e904065db2
namwook <tomato_zam@naver.com>
2025-05-07 10:36:38 +0900

    클린 코드 적용

--- Project9/SimilarityChecker.cpp
+++ Project9/SimilarityChecker.cpp
@@ -1,10 +1,9 @@
 #include <string>
-#include <cstdlib> // for std::abs
 using namespace std;
 
 class SimilarityChecker {
 public:
-    int getLengthAndScore(string inputA, string inputB) {
+    int getLengthAndScore(const string inputA, const string inputB) const {
         int lengthA = inputA.length();
         int lengthB = inputB.length();
 
